20fb628825d3ba41a6c0ed39ef009431730bed47
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-14 16:27:58 +0200

    add test function

---
+++ CryptoWallet/CryptoWallet/Application/AppShellViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace CryptoWallet
+{
+    public class AppShellViewModel
+    {
+        public ICommand SignOutCommand { get => new Command(async () => await SignOut()); }
+
+        private async Task SignOut()
+        {
+            await Shell.Current.DisplayAlert("WARNING!!!", "Logout?", "Yes");
+        }
+    }
+}
