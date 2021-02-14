5de07ad17f8acbfb7854b60d6b07ac425fac0732
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-14 20:40:15 +0200

    add BaseViewModel

---
+++ CryptoWallet/CryptoWallet/Common/Base/BaseViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoWallet.Common.Base
+{
+    public abstract class BaseViewModel : ExtendedBindableObject
+    {
+        public virtual Task InitializeAsync(object parameter)
+        {
+            return Task.CompletedTask;
+        }
+
+        private bool _isBusy;
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                if (SetProperty(ref _isBusy, value))
+                {
+                    IsNotBusy = !_isBusy;
+                }
+            }
+        }
+
+        private bool _isNotBusy = true;
+        public bool IsNotBusy
+        {
+            get => _isNotBusy;
+            set
+            {
+                if (SetProperty(ref _isNotBusy, value))
+                {
+                    IsBusy = !_isNotBusy;
+                }
+            }
+        }
+    }
+}
