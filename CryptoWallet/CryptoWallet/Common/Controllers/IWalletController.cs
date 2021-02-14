02c98a6545621a55f9b57ec552710142d84f422a
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-14 20:40:48 +0200

    add WalletController

---
+++ CryptoWallet/CryptoWallet/Common/Controllers/IWalletController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using CryptoWallet.Common.Models;
+
+namespace CryptoWallet.Common.Controllers
+{
+    public interface IWalletController 
+    {
+        Task<List<Coin>> GetCoins(bool forceReload = false);
+    }
+}
---
+++ CryptoWallet/CryptoWallet/Common/Controllers/WalletController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using CryptoWallet.Common.Models;
+
+namespace CryptoWallet.Common.Controllers
+{
+    public class WalletController : IWalletController
+    {
+        private List<Coin> defaultAssets = new()
+        {
+            new Coin
+            {
+                Name = "Bitcoin",
+                Amount = 1,
+                Symbol = "BTC",
+                DollarValue = 9500
+            },
+            new Coin
+            {
+                Name = "Ethereum",
+                Amount = 2,
+                Symbol = "ETH",
+                DollarValue = 300
+            },
+            new Coin
+            {
+                Name = "Litecoin",
+                Amount = 3,
+                Symbol = "LTC",
+                DollarValue = 150
+            },
+        };
+        public Task<List<Coin>> GetCoins(bool forceReload = false)
+        {
+            return Task.FromResult(this.defaultAssets);
+        }
+    }
+}
