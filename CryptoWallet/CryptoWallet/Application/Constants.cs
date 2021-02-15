4cb40765447f267832810863289d2eea90721b5c
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-15 23:27:21 +0200

    add Constans class

---
+++ CryptoWallet/CryptoWallet/Application/Constants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoWallet
+{
+    public class Constants
+    {
+        public const string TRANSACTION_DEPOSITED = "Deposited";
+
+        public const string TRANSACTION_WITHDRAWN = "Withdrawn";
+        
+        public const string TRANSACTION_DEPOSITED_IMAGE = "deposited.png";
+        
+        public const string TRANSACTION_WITHDRAWN_IMAGE = "withdrawn.png";
+    }
+}
