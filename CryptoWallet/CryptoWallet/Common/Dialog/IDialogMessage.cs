96ec5fe1f5e7afe2f7cbcb00e1e8c9498e220a79
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-17 20:37:27 +0200

    add dialog msgs

---
+++ CryptoWallet/CryptoWallet/Common/Dialog/IDialogMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace CryptoWallet.Common.Dialog
+{
+    public interface IDialogMessage
+    {
+        Task DisplayAlert(string title, string message, string cancel);
+
+        Task<bool> DisplayAlert(string title, string message, string accept, string cancel);
+
+        Task<string> DisplayPrompt(string title, string message, string accept, string cancel);
+
+        Task<string> DisplayActionSheet(string title, string destruction, params string[] buttons);
+    }
+}
