9a02f70227bc60b91419f1b0ac9cfb2fd911cd82
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-17 17:08:05 +0200

    add validatable object class

---
+++ CryptoWallet/CryptoWallet/Common/Validations/ValidatableObject.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using CryptoWallet.Common.Base;
+
+namespace CryptoWallet.Common.Validations
+{
+    public class ValidatableObject<T> : ExtendedBindableObject
+    {
+        public List<IValidationRule<T>> Validations { get; }
+
+        private List<string> errors;
+
+        public List<string> Errors
+        {
+            get => this.errors;
+            set { SetProperty(ref this.errors, value); }
+        }
+
+        private T _value;
+
+        public T Value
+        {
+            get => this._value;
+            set { SetProperty(ref this._value, value); }
+        }
+
+        private bool isValid;
+
+        public bool IsValid
+        {
+            get => this.isValid;
+            set { SetProperty(ref this.isValid, value); }
+        }
+
+
+        public ValidatableObject()
+        {
+            this.isValid = true;
+            this.errors = new List<string>();
+            this.Validations = new List<IValidationRule<T>>();
+        }
+
+        public bool Validate()
+        {
+            this.Errors.Clear();
+
+            IEnumerable<string> errors = this.Validations.Where(v => !v.Check(this.Value))
+                .Select(v => v.ValidationMessage);
+
+            this.Errors = errors.ToList();
+            this.IsValid = !this.Errors.Any();
+
+            return this.IsValid;
+        }
+    }
+}
