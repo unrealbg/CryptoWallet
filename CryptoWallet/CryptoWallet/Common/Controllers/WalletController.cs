a1621902fdc3d99be1ade884768c2fdfd4203d9c
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-18 00:18:20 +0200

    delete test data + code improvements

--- CryptoWallet/CryptoWallet/Common/Controllers/WalletController.cs
+++ CryptoWallet/CryptoWallet/Common/Controllers/WalletController.cs
@@ -1,87 +1,108 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using CryptoWallet.Common.Database;
 using CryptoWallet.Common.Models;
+using CryptoWallet.Common.Network;
 
 namespace CryptoWallet.Common.Controllers
 {
     public class WalletController : IWalletController
     {
+        private IRepository<Transaction> transactionRepository;
+        private ICryptoService cryptoService;
+        private List<Coin> cachedCoins = new();
         private List<Coin> defaultAssets = new()
         {
             new Coin
             {
                 Name = "Bitcoin",
-                Amount = 1,
+                Amount = 0M,
                 Symbol = "BTC",
-                DollarValue = 9500
+                DollarValue = 0
             },
             new Coin
             {
                 Name = "Ethereum",
-                Amount = 2,
+                Amount = 0,
                 Symbol = "ETH",
-                DollarValue = 600
+                DollarValue = 0
             },
             new Coin
             {
                 Name = "Litecoin",
-                Amount = 3,
+                Amount = 0,
                 Symbol = "LTC",
-                DollarValue = 150
+                DollarValue = 0
             },
         };
-        public Task<List<Coin>> GetCoins(bool forceReload = false)
+        public WalletController(IRepository<Transaction> transactionRepository, ICryptoService cryptoService)
         {
-            return Task.FromResult(this.defaultAssets);
+            this.transactionRepository = transactionRepository;
+            this.cryptoService = cryptoService;
+        }
+        public async Task<List<Coin>> GetCoins(bool forceReload = false)
+        {
+            List<Transaction> transactions = await LoadTransactions(forceReload);
+            if (transactions.Count == 0 || this.cachedCoins.Count == 0)
+            {
+                return this.defaultAssets;
+            }
+
+            var groupedTransactions = transactions.GroupBy(x => x.Symbol);
+            List<Coin> result = new();
+
+            foreach (var item in groupedTransactions)
+            {
+                decimal amount = item.Where(x => x.Status == Constants.TRANSACTION_DEPOSITED).Sum(x => x.Amount)
+                               - item.Where(x => x.Status == Constants.TRANSACTION_WITHDRAWN).Sum(x => x.Amount);
+
+                Coin newCoin = new()
+                {
+                    Symbol = item.Key,
+                    Amount = amount,
+                    DollarValue = amount * (decimal)this.cachedCoins.FirstOrDefault(x => x.Symbol == item.Key).Price,
+                    Name = Coin.GetAvailableAssets().FirstOrDefault(x => x.Symbol == item.Key).Name
+                };
+
+                result.Add(newCoin);
+            }
+
+            return result.OrderByDescending(x => x.DollarValue).ToList();
         }
 
-        public Task<List<Transaction>> GetTransactions(bool foreceReload = false)
+        public async Task<List<Transaction>> GetTransactions(bool foreceReload = false)
         {
-            return Task.FromResult(new List<Transaction>
+            List<Transaction> transactions = await LoadTransactions(foreceReload);
+            if (transactions.Count == 0 || this.cachedCoins.Count == 0)
             {
-                new Transaction
-                {
-                    Amount = 1,
-                    DollarValue = 9500,
-                    Status = Constants.TRANSACTION_DEPOSITED,
-                    StatusImageSource = Constants.TRANSACTION_DEPOSITED_IMAGE,
-                    Symbol = "BTC",
-                    TransactionDate = DateTime.UtcNow
-                },
+                return transactions;
+            }
 
-                new Transaction
-                {
-                    Amount = 2,
-                    DollarValue = 600,
-                    Status = Constants.TRANSACTION_DEPOSITED,
-                    StatusImageSource = Constants.TRANSACTION_DEPOSITED_IMAGE,
-                    Symbol = "ETH",
-                    TransactionDate = DateTime.UtcNow
-                },
-
-                new Transaction
-                {
-                    Amount = 3,
-                    DollarValue = 150,
-                    Status = Constants.TRANSACTION_DEPOSITED,
-                    StatusImageSource = Constants.TRANSACTION_DEPOSITED_IMAGE,
-                    Symbol = "LTC",
-                    TransactionDate = DateTime.UtcNow
-                },
-
-                new Transaction
-                {
-                    Amount = 1,
-                    DollarValue = 40,
-                    Status = Constants.TRANSACTION_WITHDRAWN,
-                    StatusImageSource = Constants.TRANSACTION_WITHDRAWN_IMAGE,
-                    Symbol = "LTC",
-                    TransactionDate = DateTime.UtcNow
-                },
+            transactions.ForEach(t =>
+            {
+                t.StatusImageSource = t.Status == Constants.TRANSACTION_DEPOSITED ?
+                                            Constants.TRANSACTION_DEPOSITED_IMAGE :
+                                            Constants.TRANSACTION_WITHDRAWN_IMAGE;
+                t.DollarValue = t.Amount * (decimal)this.cachedCoins.First(x => x.Symbol == t.Symbol).Price;
             });
+
+            return transactions;
+        }
+
+        private async Task<List<Transaction>> LoadTransactions(bool forceReload)
+        {
+            if (this.cachedCoins.Count == 0 || forceReload)
+            {
+                this.cachedCoins = await this.cryptoService.GetLatestPrices();
+            }
+
+            var transactions = await this.transactionRepository.GetAllAsync();
+            transactions = transactions.OrderByDescending(x => x.TransactionDate).ToList();
+            return transactions;
         }
     }
 }
