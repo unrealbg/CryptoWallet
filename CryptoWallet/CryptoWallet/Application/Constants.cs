3c1a0bf8be2698d1c3070086accc41772e841d9e
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-18 00:07:48 +0200

    add new constant for crypto api

--- CryptoWallet/CryptoWallet/Application/Constants.cs
+++ CryptoWallet/CryptoWallet/Application/Constants.cs
@@ -13,5 +13,8 @@ namespace CryptoWallet
         public const string TRANSACTION_DEPOSITED_IMAGE = "deposited.png";
         
         public const string TRANSACTION_WITHDRAWN_IMAGE = "withdrawn.png";
+
+
+        public const string CRYPTO_API = "https://api.coingecko.com/api/v3/";
     }
 }
