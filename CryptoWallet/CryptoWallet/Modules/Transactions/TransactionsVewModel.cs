b30a4f084b758061e1090f8047b07718c2cb6dce
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-17 13:57:17 +0200

    impl nav service

--- CryptoWallet/CryptoWallet/Modules/Transactions/TransactionsVewModel.cs
+++ CryptoWallet/CryptoWallet/Modules/Transactions/TransactionsVewModel.cs
@@ -9,6 +9,9 @@ using System.Windows.Input;
 using CryptoWallet.Common.Base;
 using CryptoWallet.Common.Controllers;
 using CryptoWallet.Common.Models;
+using CryptoWallet.Common.Navigation;
+using CryptoWallet.Modules.AddTransaction;
+using CryptoWallet.Modules.Wallet;
 
 using Xamarin.Forms;
 
@@ -17,11 +20,13 @@ namespace CryptoWallet.Modules.Transactions
     public class TransactionsVewModel : BaseViewModel
     {
         private IWalletController walletController;
+        private INavigationService navigationService;
         private string filter = string.Empty;
 
-        public TransactionsVewModel(IWalletController walletController)
+        public TransactionsVewModel(IWalletController walletController, INavigationService navigationService)
         {
             this.walletController = walletController;
+            this.navigationService = navigationService;
             this.Transactions = new ObservableCollection<Transaction>();
         }
 
@@ -33,14 +38,14 @@ namespace CryptoWallet.Modules.Transactions
 
         private async Task GetTransactions()
         {
-            this.IsRefreshin = true;
+            this.IsRefreshing = true;
             List<Transaction> transactions = await this.walletController.GetTransactions();
             if (!string.IsNullOrEmpty(this.filter))
             {
                 transactions = transactions.Where(t => t.Status == this.filter).ToList();
             }
             this.Transactions = new ObservableCollection<Transaction>(transactions);
-            this.IsRefreshin = false;
+            this.IsRefreshing = false;
         }
 
         private ObservableCollection<Transaction> transactions;
@@ -61,7 +66,7 @@ namespace CryptoWallet.Modules.Transactions
 
         private bool isRefreshing;
 
-        public bool IsRefreshin
+        public bool IsRefreshing
         {
             get => this.isRefreshing;
             set { SetProperty(ref this.isRefreshing, value); }
@@ -78,7 +83,14 @@ namespace CryptoWallet.Modules.Transactions
 
         private async Task TransactionSelected()
         {
+            await this.navigationService.PushAsync<AddTransactionViewModel>("id=1");
+        }
 
+        public ICommand TradeCommand { get => new Command(async () => await PerformNavigation()); }
+
+        private async Task PerformNavigation()
+        {
+            await this.navigationService.PushAsync<AddTransactionViewModel>();
         }
 
     }
