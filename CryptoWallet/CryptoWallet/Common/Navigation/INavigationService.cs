983ced5e08178dcee41d2d699da6ca30b9607626
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-17 13:55:49 +0200

    add navigation service

---
+++ CryptoWallet/CryptoWallet/Common/Navigation/INavigationService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using CryptoWallet.Common.Base;
+
+namespace CryptoWallet.Common.Navigation
+{
+    public interface INavigationService
+    {
+        Task PushAsync<TViewModel>(string parameters = null) where TViewModel : BaseViewModel;
+        
+        Task PopAsync();
+
+        Task InsertAsRoot<TViewModel>(string parameters = null) where TViewModel : BaseViewModel;
+    }
+}
---
+++ CryptoWallet/CryptoWallet/Common/Navigation/ShellRoutingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using CryptoWallet.Common.Base;
+
+using Xamarin.Forms;
+
+namespace CryptoWallet.Common.Navigation
+{
+    public class ShellRoutingService : INavigationService
+    {
+        public Task PopAsync()
+        {
+            return Shell.Current.Navigation.PopAsync();
+        }
+
+        public Task InsertAsRoot<TViewModel>(string parameters = null) where TViewModel : BaseViewModel
+        {
+            return GoToAsync<TViewModel>("//", parameters);
+        }
+
+
+        public Task PushAsync<TViewModel>(string parameters = null) where TViewModel : BaseViewModel
+        {
+            return GoToAsync<TViewModel>("", parameters);
+        }
+
+        private Task GoToAsync<TViewModel>(string routePrefix, string parameters) where TViewModel : BaseViewModel
+        {
+            string route = routePrefix + typeof(TViewModel).Name;
+
+            if (!string.IsNullOrWhiteSpace(parameters))
+            {
+                route += $"?{parameters}";
+            }
+
+            return Shell.Current.GoToAsync(route);
+        }
+    }
+}
