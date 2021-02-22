7c1bd0954c4bfbc66aa81c3d43105be583f9e736
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-22 12:13:15 +0200

    add loading view

---
+++ CryptoWallet/CryptoWallet/Modules/Loading/LoadingView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CryptoWallet.Modules.Loading
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoadingView : ContentPage
+    {
+        public LoadingView()
+        {
+            InitializeComponent();
+            BindingContext = App.Container.Resolve<LoadingViewModel>();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await (BindingContext as LoadingViewModel).InitializeAsync(null);
+        }
+    }
+}
---
+++ CryptoWallet/CryptoWallet/Modules/Loading/LoadingViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+
+using CryptoWallet.Common.Base;
+using CryptoWallet.Common.Navigation;
+using CryptoWallet.Modules.Login;
+
+using Xamarin.Essentials;
+
+namespace CryptoWallet.Modules.Loading
+{
+    public class LoadingViewModel : BaseViewModel
+    {
+        private INavigationService navigationService;
+
+        public LoadingViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        public override Task InitializeAsync(object parameter)
+        {
+            if (!Preferences.ContainsKey(Constants.SHOWN_ONBOARDING))
+            {
+                Preferences.Set(Constants.SHOWN_ONBOARDING, true);
+                this.navigationService.GoToLoginFlow();
+
+                return Task.CompletedTask;
+            }
+
+            if (Preferences.ContainsKey(Constants.IS_USER_LOGGED_IN) && Preferences.Get(Constants.IS_USER_LOGGED_IN, false) == true)
+            {
+                this.navigationService.GoToMainFlow();
+
+                return Task.CompletedTask;
+            }
+
+            this.navigationService.GoToLoginFlow();
+            return this.navigationService.InsertAsRoot<LoginViewModel>();
+        }
+    }
+}
