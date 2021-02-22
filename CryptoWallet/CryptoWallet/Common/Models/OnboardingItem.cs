72dca10cc231195777aac6010fe4d751cc60b7b8
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-22 12:13:33 +0200

    add onboarding model

---
+++ CryptoWallet/CryptoWallet/Common/Models/OnboardingItem.cs
@@ -0,0 +1,17 @@
+﻿namespace CryptoWallet.Common.Models
+{
+    public class OnboardingItem
+    {
+        public OnboardingItem(string imageUrl, string title, string details)
+        {
+            this.ImageUrl = imageUrl;
+            this.Title = title;
+            this.Details = details;
+        }
+
+        public string Details { get; set; }
+        public string ImageUrl { get; set; }
+
+        public string Title { get; set; }
+    }
+}
