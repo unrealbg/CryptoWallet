fee9581cbb36f9d223d0c6f999358c2e699fb3e8
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-22 12:24:32 +0200

    add login shell

---
+++ CryptoWallet/CryptoWallet/Application/LoginShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CryptoWallet
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginShell : Shell
+    {
+        public LoginShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
