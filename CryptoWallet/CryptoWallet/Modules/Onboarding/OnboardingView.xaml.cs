c502e6dd8ba3f6fadc2bd5d34014f802e7a38aac
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-22 12:13:03 +0200

    add onboarding option

---
+++ CryptoWallet/CryptoWallet/Modules/Onboarding/OnboardingView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CryptoWallet.Modules.Onboarding
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OnboardingView : ContentPage
+    {
+        public OnboardingView()
+        {
+            InitializeComponent();
+            BindingContext = App.Container.Resolve<OnboardingViewModel>();
+        }
+    }
+}
---
+++ CryptoWallet/CryptoWallet/Modules/Onboarding/OnboardingViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+using CryptoWallet.Common.Base;
+using CryptoWallet.Common.Models;
+using CryptoWallet.Common.Navigation;
+using CryptoWallet.Modules.Login;
+
+using Xamarin.Forms;
+
+namespace CryptoWallet.Modules.Onboarding
+{
+    public class OnboardingViewModel : BaseViewModel
+    {
+        private INavigationService navigationService;
+
+        public OnboardingViewModel(INavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
+        public ObservableCollection<OnboardingItem> OnboardingSteps { get; set; } = new ObservableCollection<OnboardingItem>
+        {
+            new OnboardingItem("welcome.png", "Welcome to CryptoWallet", "Manage all your crypto assets! It's simple and easy!"),
+            new OnboardingItem("nice.png", "Nice and Tidy Crypto Portfolio", "Keep BTC, ETH, XRP and many other tokens."),
+            new OnboardingItem("security.png", "Your Safety is Our Top Priority", "On top-notch security features will keep you completely safe.")
+        };
+
+        public ICommand SkipCommand { get => new Command(async () => await Skip()); }
+
+        private async Task Skip()
+        {
+            await this.navigationService.InsertAsRoot<LoginViewModel>();
+        }
+    }
+}
