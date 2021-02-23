cab7e2f791943bc37cf8536f086cd8e3d4cbf0b9
Zhelyazko Zhelyazkov <jack@unrealbg.com>
2021-02-24 00:28:02 +0200

    add User model

---
+++ CryptoWallet/CryptoWallet/Common/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using CryptoWallet.Common.Database;
+
+namespace CryptoWallet.Common.Models
+{
+    public class User : BaseDatabaseItem
+    {
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
